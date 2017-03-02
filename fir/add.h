808754083923da65060bb55901e8878a7440829d
wangsdchn <wangsdbit@163.com>
2017-03-02 16:45:00 +0800

    fir

---
+++ fir/add.h
@@ -0,0 +1,11 @@
+#ifndef ADD_H
+#define ADD_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+	extern int add(int a, int b);
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ fir/fir.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include "add.h"
+using namespace cv;
+using namespace std;
+int main()
+{
+	int a = 2;
+	int b = 6;
+#ifndef _DEBUG
+	cout << "Debug" << endl;
+#endif
+	Mat src = imread("0000.png");
+	imshow("src",src);
+	waitKey(0);
+	//printf("%d\n",add(a,b));
+	return 0;
+}
