317172cdc3694bb8c1d0a038947c9208f08238eb
wangsdchn <wangsdbit@163.com>
2017-04-19 09:48:45 +0800

    指针，开辟空间，memset，openmp测试。开辟空间变慢，memset变快，但效果不是特别明显

--- fir/main.cpp
+++ fir/main.cpp
@@ -1,3 +1,5 @@
+
+#include<omp.h>
 #include "mytest.h"
 #include "add.h"
 #include<time.h>
@@ -17,20 +19,40 @@ int main()
 	test1.sqrtNum = 3.0;
 	int a;
 	float b;
-	//======================================
-	QueryPerformanceCounter(&m_StartTime);
-	for (int i = 0; i < 1000000; i++)
-		b = test1.QuickInvSqrt(test1.sqrtNum);
-	QueryPerformanceCounter(&m_EndTime);
-	cout << b << " "<<(double)(m_EndTime.QuadPart-m_StartTime.QuadPart)/m_Freq.QuadPart << endl;
+	////======================================
+	//QueryPerformanceCounter(&m_StartTime);
+	//for (int i = 0; i < 1000000; i++)
+	//	b = test1.QuickInvSqrt(test1.sqrtNum);
+	//QueryPerformanceCounter(&m_EndTime);
+	//cout << b << " "<<(double)(m_EndTime.QuadPart-m_StartTime.QuadPart)/m_Freq.QuadPart << endl;
 
-	//=======================================
-	QueryPerformanceCounter(&m_StartTime);
-	for (int i = 0; i < 1000000; i++)
-		b = 1/sqrtf(test1.sqrtNum);
-	QueryPerformanceCounter(&m_EndTime);
-	cout << b << " " << (double)(m_EndTime.QuadPart - m_StartTime.QuadPart) / m_Freq.QuadPart << endl;
+	////=======================================
+	//QueryPerformanceCounter(&m_StartTime);
+	//for (int i = 0; i < 1000000; i++)
+	//	b = 1/sqrtf(test1.sqrtNum);
+	//QueryPerformanceCounter(&m_EndTime);
+	//cout << b << " " << (double)(m_EndTime.QuadPart - m_StartTime.QuadPart) / m_Freq.QuadPart << endl;
+	int * p[8];
 
+	QueryPerformanceCounter(&m_StartTime);
+	for (int i = 0; i < 8; i++)
+	{
+		p[i] = new int[1280*1024*50];
+	}
+	QueryPerformanceCounter(&m_EndTime);
+	cout <<(double)(m_EndTime.QuadPart-m_StartTime.QuadPart)/m_Freq.QuadPart << endl;
+
+	QueryPerformanceCounter(&m_StartTime);
+
+#ifdef USE_OPENMP
+#pragma omp parallel for
+#endif
+	for (int i = 0; i < 8; i++)
+	{
+		memset(p[i], 0, 1280 * 1024*50*sizeof(int));
+	}
+	QueryPerformanceCounter(&m_EndTime);
+	cout << (double)(m_EndTime.QuadPart - m_StartTime.QuadPart) / m_Freq.QuadPart << endl;
 	system("pause");
 	return 0;
 }
