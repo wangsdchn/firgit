60ea39f3bf489592bc13d0486ad71c060b353388
wangsdchn <wangsdbit@163.com>
2017-07-28 13:25:01 +0800

    windbg

--- fir/main.cpp
+++ fir/main.cpp
@@ -9,12 +9,22 @@
 using namespace cv;
 using namespace std;
 void ch(int *p){ cout << *p << endl; int *p1; p1 = p; *p1 = 4; cout << *p << endl; };
+void allocateMemory()
+{
+	int *p = new int[4000];
+	ZeroMemory(p, 16000);
+	Sleep(1);
+}
 int main()
 {
-	string fileName = "E:\\gamma.txt";
-	float gamma = 1.25;
-	FileOps::CFileOp file(gamma,fileName);
-	file.getResult();
- 	system("pause");
+	//string fileName = "E:\\gamma.txt";
+	//float gamma = 1.25;
+	//FileOps::CFileOp file(gamma,fileName);
+	//file.getResult();
+ //	system("pause");
+	while (1)
+	{
+		allocateMemory();
+	}
 	return 0;
 } 
