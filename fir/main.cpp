d76dee5ccf4b38790f80288a7af213a68610743f
wangsdchn <wangsdbit@163.com>
2017-08-14 15:07:32 +0800

    assert

--- fir/main.cpp
+++ fir/main.cpp
@@ -6,6 +6,7 @@
 #include<time.h>
 #include<vector>
 #include <windows.h>
+#include <assert.h>
 using namespace cv;
 using namespace std;
 void ch(int *p){ cout << *p << endl; int *p1; p1 = p; *p1 = 4; cout << *p << endl; };
@@ -15,16 +16,26 @@ void allocateMemory()
 	ZeroMemory(p, 16000);
 	Sleep(1);
 }
+void sizeOfPtr(char *p)
+{
+	assert(p != NULL);
+	cout << strlen(p) << " " << sizeof(p) << endl;
+}
 int main()
 {
 	//string fileName = "E:\\gamma.txt";
 	//float gamma = 1.25;
 	//FileOps::CFileOp file(gamma,fileName);
 	//file.getResult();
- //	system("pause");
-	while (1)
-	{
-		allocateMemory();
-	}
+	//	
+	char *p = new char[10];
+	memset(p, 0, 9*sizeof(char));
+	p[0] = 'a';
+	p[1] = 'a';
+	p[9] = '\0';
+	cout << strlen(p) << " " << sizeof(p) << endl;
+	sizeOfPtr(p);
+
+	system("pause");
 	return 0;
 } 
