e23fa8aa1e562280fffd0cb13138767c6e729d14
wangsdchn <wangsdbit@163.com>
2017-10-22 22:53:10 +0800

    add dll test

--- fir/main.cpp
+++ fir/main.cpp
@@ -1,16 +1,19 @@
-
 #include<omp.h>
 #include "mytest.h"
-#include "add.h"
 #include "fileOps.h"
 #include<time.h>
 #include<vector>
+<<<<<<< HEAD
+#include <windows.h>
+=======
 //#include <windows.h>
 #define _AFXDLL
 #include <afxtempl.h>
 #include <assert.h>
 using namespace cv;
+>>>>>>> 6aceb2d8e26b980b7693ececf0aaf4196b40576c
 using namespace std;
+typedef int(*ApiAdd)(int, int);	//add function
 void ch(int *p){ cout << *p << endl; int *p1; p1 = p; *p1 = 4; cout << *p << endl; };
 void allocateMemory()
 {
@@ -25,6 +28,24 @@ void sizeOfPtr(char *p)
 }
 int main()
 {
+<<<<<<< HEAD
+	HINSTANCE hdll = LoadLibrary("..\\Debug\\add.dll");
+	if (hdll == NULL)
+	{
+		FreeLibrary(hdll);
+	}
+	ApiAdd add=(ApiAdd)GetProcAddress(hdll, "add");
+	if (add == NULL)
+	{
+		FreeLibrary(hdll);
+	}
+	int a, b, c;
+	cout << "Input two numbers:";
+	cin >> a >> b;
+	c = add(a, b);
+	cout << c << endl;
+ 	system("pause");
+=======
 	//string fileName = "E:\\gamma.txt";
 	//float gamma = 1.25;
 	//FileOps::CFileOp file(gamma,fileName);
@@ -35,5 +56,6 @@ int main()
 	cout << s.str() << endl;
 
 	system("pause");
+>>>>>>> 6aceb2d8e26b980b7693ececf0aaf4196b40576c
 	return 0;
-} 
+}
