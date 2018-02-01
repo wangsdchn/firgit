8a664daf06ac8830a246ab0d333ea9adb1ef4a25
wangsdchn <wangsdbit@163.com>
2018-02-01 18:01:04 +0800

    caffe classification

--- fir/main.cpp
+++ fir/main.cpp
@@ -1,121 +1,35 @@
-
 #include<omp.h>
 #include "mytest.h"
 #include "fileOps.h"
 #include<time.h>
 #include<vector>
-//#include <windows.h>
-#define _AFXDLL
-#include <afxtempl.h>
-#include <assert.h>
+#include "classification.h"
 
-#include <WinInet.h>
-#include <SensAPI.h>
-using namespace cv;
-using namespace std;
-
-
-double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-	size_t size1 = nums1.size();
-	size_t size2=nums2.size();
-	size_t newsize=0,midloc=0,sortedsize=0,index1=0,index2=0;
-	int tempnum=0;
-	double midnum = 0.0;
-
-	newsize = size1 + size2;
-	bool bfind = false;
-	midloc = newsize >> 1;
-	int tempnum1 = 0, midindex = 0;
-	while (true)
-	{
-		while (index1 < size1 && index2<size2 && !bfind)
-		{
-			if (nums1[index1] >= nums2[index2])
-			{
-				tempnum1 = tempnum;
-				tempnum = nums2[index2];
-				if (midindex == midloc)
-				{
-					bfind = true;
-					break;
-				}
-				else
-				{
-					index2++;
-					midindex++;
-				}
-			}
-			else
-			{
-				tempnum1 = tempnum;
-				tempnum = nums1[index1];
-				if (midindex == midloc)
-				{
-					bfind = true;
-					break;
-				}
-				else
-				{
-					index1++;
-					midindex++;
-				}
-			}
-		}
-		while (index2 < size2 && !bfind)
-		{
-			tempnum1 = tempnum;
-			tempnum = nums2[index2];
-			if (midindex == midloc)
-			{
-				bfind = true;
-				break;
-			}
-			else
-			{
-				index2++;
-				midindex++;
-			}
-		}
-		while (index1 < size1 && !bfind)
-		{
-			tempnum1 = tempnum;
-			tempnum = nums1[index1];
-			if (midindex == midloc)
-			{
-				bfind = true;
-				break;
-			}
-			else
-			{
-				index1++;
-				midindex++;
-			}
-		}
-		if (bfind)
-			break;
-	}
-	cout << (newsize & 0x1) << endl;
-	if (0==(newsize & 0x1))
-		midnum = 1.0*(tempnum + tempnum1) / 2;
-	else
-		midnum = tempnum;
-
-	return midnum;
-}
 int main()
 {
-	int x;
-	int bit1num = 0;
-	while (1)
-	{
-		bit1num = 0;
-		cin >> x;
-		while (x)
-		{
-			x = x&(x - 1);
-			bit1num++;
-		}
-		cout << bit1num << endl;
+	std::string logfile = "log.txt";
+	::google::InitGoogleLogging(logfile.c_str());
+
+	string model_file = "lenet_train_test.prototxt";
+	string trained_file = "lenet_iter_10000.caffemodel";
+	string mean_file = "mean.binaryproto";
+	string label_file = "synset_words.txt";
+	Classifier classifier(model_file, trained_file, mean_file, label_file);
+
+	string file = "00_0051.jpg";
+
+	std::cout << "---------- Prediction for "
+		<< file << " ----------" << std::endl;
+
+	cv::Mat img = cv::imread(file);
+	CHECK(!img.empty()) << "Unable to decode image " << file;
+	std::vector<Prediction> predictions = classifier.Classify(img);
+
+	/* Print the top N predictions. */
+	for (size_t i = 0; i < predictions.size(); ++i) {
+		Prediction p = predictions[i];
+		std::cout << std::fixed << std::setprecision(4) << p.second << " - \""
+			<< p.first << "\"" << std::endl;
 	}
 	system("pause");
 	return 0;
